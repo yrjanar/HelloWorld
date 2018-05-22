fd73f070a925b9d70095cb71a6cdfd2aafd4e4b2
yrjanar <39477314+yrjanar@users.noreply.github.com>
2018-05-22 09:12:43 +0300

    Update HeippaTaas.java
    Vastaus lisätty koodiin.

--- HeippaTaas.java
+++ HeippaTaas.java
@@ -4,5 +4,6 @@ public class HeippaTaas {
     public static void main(String[] args) {
         // toteuta tänne ohjelma, joka tulostaa "Heippa taas!"
         System.out.println("Heippa taas!");
+        System.out.println("No hei vaan!");
     }
 }
